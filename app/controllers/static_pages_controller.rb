bc231dc99c66833bb4cc2c5bd93b7abc658515ae
Drew Hund <languageinvestigator@gmail.com>
2020-09-22 19:39:57 -0400

    finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,20 @@
 class StaticPagesController < ApplicationController
-  require 'odin-flickr/config/initializers/flickr.rb'
+  require 'flickraw'
 
   def home
-    @flickr = FlickRaw::Flickr.new
+    FlickRaw.api_key = ENV["FLICKRAW_API_KEY"]
+    FlickRaw.shared_secret = ENV["FLICKRAW_SHARED_SECRET"]
 
+    flickr = FlickRaw::Flickr.new
+
+    unless params[:flickr_id].nil?
+      @photos = flickr.people.getPhotos(user_id: params[:flickr_id])
+      @urls = []
+      @photos.each do |photo|
+        @urls << "https://farm#{photo.farm}.staticflickr.com/#{photo.server}/#{photo.id}_#{photo.secret}.jpg"
+      end
+    end
 
   end
+
 end
